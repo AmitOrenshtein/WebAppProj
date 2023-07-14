6ed087a4bd0c571b552b85287715c8aed8ccbd83
Nadav levy <69619783+nadavlevy40@users.noreply.github.com>
2023-07-14 08:40:24 +0300

    edited

--- public/adminInventory/index.js
+++ public/adminInventory/index.js
@@ -1,174 +1,260 @@
-
 // Function to handle product creation form submission
 function createProduct(event) {
-    event.preventDefault();
-    const name = document.getElementById('product-name').value;
-  
-    // Send a request to the backend API to create the product
-    fetch('/api/products', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: name,
-        // Include other form data in the request body
-      }),
+  event.preventDefault();
+  const name = document.getElementById('product-name').value;
+
+  // Send a request to the backend API to create the product
+  fetch('/api/products', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      // Include other form data in the request body
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response from the backend
+      // For example, update the product list display with the newly created product
+      fetchProducts();
     })
-      .then(response => response.json())
-      .then(data => {
-        // Handle the response from the backend
-        // For example, update the product list display with the newly created product
-        fetchProducts();
-      })
-      .catch(error => {
-        // Handle errors
-        console.error('Error creating product:', error);
-      });
-  }
-  
-  // Function to handle supplier creation form submission
-  function createSupplier(event) {
-    event.preventDefault();
-    const name = document.getElementById('supplier-name').value;
-    // Retrieve other form data as needed
-  
-    // Send a request to the backend API to create the supplier
-    fetch('/api/suppliers', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: name,
-        // Include other form data in the request body
-      }),
+    .catch(error => {
+      // Handle errors
+      console.error('Error creating product:', error);
+    });
+}
+
+// Function to handle supplier creation form submission
+function createSupplier(event) {
+  event.preventDefault();
+  const name = document.getElementById('supplier-name').value;
+  // Retrieve other form data as needed
+
+  // Send a request to the backend API to create the supplier
+  fetch('/api/suppliers', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      // Include other form data in the request body
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response from the backend
+      // For example, update the supplier list display with the newly created supplier
+      fetchSuppliers();
     })
-      .then(response => response.json())
-      .then(data => {
-        // Handle the response from the backend
-        // For example, update the supplier list display with the newly created supplier
-        fetchSuppliers();
-      })
-      .catch(error => {
-        // Handle errors
-        console.error('Error creating supplier:', error);
-      });
-  }
-  
-  // Function to handle branch creation form submission
-  function createBranch(event) {
-    event.preventDefault();
-    const name = document.getElementById('branch-name').value;
-    // Retrieve other form data as needed
-  
-    // Send a request to the backend API to create the branch
-    fetch('/api/branches', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: name,
-        // Include other form data in the request body
-      }),
+    .catch(error => {
+      // Handle errors
+      console.error('Error creating supplier:', error);
+    });
+}
+
+// Function to handle branch creation form submission
+function createBranch(event) {
+  event.preventDefault();
+  const name = document.getElementById('branch-name').value;
+  // Retrieve other form data as needed
+
+  // Send a request to the backend API to create the branch
+  fetch('/api/branches', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name,
+      // Include other form data in the request body
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response from the backend
+      // For example, update the branch list display with the newly created branch
+      fetchBranches();
     })
-      .then(response => response.json())
-      .then(data => {
-        // Handle the response from the backend
-        // For example, update the branch list display with the newly created branch
-        fetchBranches();
-      })
-      .catch(error => {
-        // Handle errors
-        console.error('Error creating branch:', error);
+    .catch(error => {
+      // Handle errors
+      console.error('Error creating branch:', error);
+    });
+}
+
+// Function to handle product deletion
+function deleteProduct(productId) {
+  // Send a request to the backend API to delete the product
+  fetch(`/api/products/${productId}`, {
+    method: 'DELETE',
+  })
+    .then(response => {
+      // Handle the response from the backend
+      // For example, update the product list display after deletion
+      fetchProducts();
+    })
+    .catch(error => {
+      // Handle errors
+      console.error('Error deleting product:', error);
+    });
+}
+
+// Function to handle product update
+function updateProduct(productId, newName) {
+  // Send a request to the backend API to update the product
+  fetch(`/api/products/${productId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: newName,
+      // Include other updated fields in the request body
+    }),
+  })
+    .then(response => {
+      // Handle the response from the backend
+      // For example, update the product list display after update
+      fetchProducts();
+    })
+    .catch(error => {
+      // Handle errors
+      console.error('Error updating product:', error);
+    });
+}
+
+// Function to handle supplier deletion
+function deleteSupplier(supplierId) {
+  // Send a request to the backend API to delete the supplier
+  fetch(`/api/suppliers/${supplierId}`, {
+    method: 'DELETE',
+  })
+    .then(response => {
+      // Handle the response from the backend
+      // For example, update the supplier list display after deletion
+      fetchSuppliers();
+    })
+    .catch(error => {
+      // Handle errors
+      console.error('Error deleting supplier:', error);
+    });
+}
+
+// Function to handle supplier update
+function updateSupplier(supplierId, newName) {
+  // Send a request to the backend API to update the supplier
+  fetch(`/api/suppliers/${supplierId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: newName,
+    }),
+  })
+    .then(response => {
+      fetchSuppliers();
+    })
+    .catch(error => {
+      console.error('Error updating supplier:', error);
+    });
+}
+
+function deleteBranch(branchId) {
+  fetch(`/api/branches/${branchId}`, {
+    method: 'DELETE',
+  })
+    .then(response => {
+      fetchBranches();
+    })
+    .catch(error => {
+      console.error('Error deleting branch:', error);
+    });
+}
+
+function updateBranch(branchId, newName) {
+  fetch(`/api/branches/${branchId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: newName,
+    }),
+  })
+    .then(response => {
+      fetchBranches();
+    })
+    .catch(error => {
+      console.error('Error updating branch:', error);
+    });
+}
+
+document.getElementById('create-product-form').addEventListener('submit', createProduct);
+document.getElementById('create-supplier-form').addEventListener('submit', createSupplier);
+document.getElementById('create-branch-form').addEventListener('submit', createBranch);
+
+function fetchProducts() {
+  fetch('/api/products')
+    .then(response => response.json())
+    .then(data => {
+      const productList = document.getElementById('product-list');
+      productList.innerHTML = '';
+
+      data.forEach(product => {
+        const productItem = document.createElement('div');
+        productItem.textContent = product.name;
+
+        productList.appendChild(productItem);
       });
-  }
-  
-  // Attach form submission event listeners
-  document.getElementById('create-product-form').addEventListener('submit', createProduct);
-  document.getElementById('create-supplier-form').addEventListener('submit', createSupplier);
-  document.getElementById('create-branch-form').addEventListener('submit', createBranch);
-  
-  // Function to fetch and display the list of products
-  function fetchProducts() {
-    // Send a request to the backend API to fetch the list of products
-    fetch('/api/products')
-      .then(response => response.json())
-      .then(data => {
-        // Update the product list display with the fetched data
-        const productList = document.getElementById('product-list');
-        productList.innerHTML = '';
-  
-        // Loop through the products and create HTML elements to display them
-        data.forEach(product => {
-          const productItem = document.createElement('div');
-          productItem.textContent = product.name;
-          // Create more HTML elements to display other product details
-  
-          productList.appendChild(productItem);
-        });
-      })
-      .catch(error => {
-        // Handle errors
-        console.error('Error fetching products:', error);
+    })
+    .catch(error => {
+      console.error('Error fetching products:', error);
+    });
+}
+
+function fetchSuppliers() {
+  fetch('/api/suppliers')
+    .then(response => response.json())
+    .then(data => {
+      const supplierList = document.getElementById('supplier-list');
+      supplierList.innerHTML = '';
+
+      data.forEach(supplier => {
+        const supplierItem = document.createElement('div');
+        supplierItem.textContent = supplier.name;
+
+        supplierList.appendChild(supplierItem);
       });
-  }
-  
-  // Function to fetch and display the list of suppliers
-  function fetchSuppliers() {
-    // Send a request to the backend API to fetch the list of suppliers
-    fetch('/api/suppliers')
-      .then(response => response.json())
-      .then(data => {
-        // Update the supplier list display with the fetched data
-        const supplierList = document.getElementById('supplier-list');
-        supplierList.innerHTML = '';
-  
-        // Loop through the suppliers and create HTML elements to display them
-        data.forEach(supplier => {
-          const supplierItem = document.createElement('div');
-          supplierItem.textContent = supplier.name;
-          // Create more HTML elements to display other supplier details
-  
-          supplierList.appendChild(supplierItem);
-        });
-      })
-      .catch(error => {
-        // Handle errors
-        console.error('Error fetching suppliers:', error);
+    })
+    .catch(error => {
+      console.error('Error fetching suppliers:', error);
+    });
+}
+
+function fetchBranches() {
+  fetch('/api/branches')
+    .then(response => response.json())
+    .then(data => {
+      const branchList = document.getElementById('branch-list');
+      branchList.innerHTML = '';
+
+      data.forEach(branch => {
+        const branchItem = document.createElement('div');
+        branchItem.textContent = branch.name;
+
+        branchList.appendChild(branchItem);
       });
-  }
-  
-  // Function to fetch and display the list of branches
-  function fetchBranches() {
-    // Send a request to the backend API to fetch the list of branches
-    fetch('/api/branches')
-      .then(response => response.json())
-      .then(data => {
-        // Update the branch list display with the fetched data
-        const branchList = document.getElementById('branch-list');
-        branchList.innerHTML = '';
-  
-        // Loop through the branches and create HTML elements to display them
-        data.forEach(branch => {
-          const branchItem = document.createElement('div');
-          branchItem.textContent = branch.name;
-          // Create more HTML elements to display other branch details
-  
-          branchList.appendChild(branchItem);
-        });
-      })
-      .catch(error => {
-        // Handle errors
-        console.error('Error fetching branches:', error);
-      });
-  }
-  
-  // Call the fetch functions when the page loads
-  window.addEventListener('load', function () {
-    fetchProducts();
-    fetchSuppliers();
-    fetchBranches();
-  });
-  
+    })
+    .catch(error => {
+      console.error('Error fetching branches:', error);
+    });
+}
+
+window.addEventListener('load', function () {
+  fetchProducts();
+  fetchSuppliers();
+  fetchBranches();
+});
--- public/admingraphs/adminGraph.js
+++ public/admingraphs/adminGraph.js
@@ -1,50 +1,61 @@
-
 async function fetchData() {
-    try {
-      const response = await fetch('/api/statistics');
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error);
-      const errorContainer = document.getElementById('error-container');
-      errorContainer.textContent = 'Error fetching data. Please try again later.';
-      return null;
+  try {
+    const response = await fetch('/api/statistics');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    const errorContainer = document.getElementById('error-container');
+    errorContainer.textContent = 'Error fetching data. Please try again later.';
+    return null;
   }
-  
-  // Function to render the graphs
-  async function renderGraphs() {
-    const data = await fetchData();
-  
-    if (!data) {
-      // Data fetch error occurred, stop rendering graphs
-      return;
+}
+
+async function renderGraphs() {
+  const data = await fetchData();
+
+  if (!data) {
+    return;
+  }
+
+  // Group by category of product
+  const categoryData = data.reduce((result, item) => {
+    if (!result[item.category]) {
+      result[item.category] = 0;
     }
-  
-    // Generate the first graph
-    const graph1Data = data.monthlyPurchases;
-    const graph1Options = {
-      title: 'Average Cumulative Average of Purchases',
-      type: 'line',
-      xLabel: 'Month',
-      yLabel: 'Average',
-    };
-    js3d.graph('graph1', graph1Data, graph1Options);
-  
-    // Generate the second graph 
-    const graph2Data = data.anotherStatistic;
-    const graph2Options = {
-      title: 'Another Statistic',
-      type: 'bar',
-      xLabel: 'Category',
-      yLabel: 'Count',
-    };
-    js3d.graph('graph2', graph2Data, graph2Options);
-  }
-  
-  // Call the renderGraphs function when the page loads
-  window.addEventListener('load', renderGraphs);
-  
+    result[item.category] += item.sales;
+    return result;
+  }, {});
+
+  const categoryGraphData = Object.entries(categoryData).map(([category, sales]) => ({ category, sales }));
+  const categoryGraphOptions = {
+    title: 'Number of Sales by Product Category',
+    type: 'bar',
+    xLabel: 'Category',
+    yLabel: 'Number of Sales',
+  };
+  js3d.graph('graph1', categoryGraphData, categoryGraphOptions);
+
+  const dateData = data.reduce((result, item) => {
+    const date = item.date.substring(0, 10); 
+    if (!result[date]) {
+      result[date] = 0;
+    }
+    result[date] += item.sales;
+    return result;
+  }, {});s
+
+  const dateGraphData = Object.entries(dateData).map(([date, sales]) => ({ date, sales }));
+  const dateGraphOptions = {
+    title: 'Number of Sales by Date',
+    type: 'line',
+    xLabel: 'Date',
+    yLabel: 'Number of Sales',
+  };
+  js3d.graph('graph2', dateGraphData, dateGraphOptions);
+}
+
+window.addEventListener('load', renderGraphs);
