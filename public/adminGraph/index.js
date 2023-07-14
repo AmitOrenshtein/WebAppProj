7d790eaf4c938568c95bbd99cf48291475c7135c
Nadav levy <69619783+nadavlevy40@users.noreply.github.com>
2023-07-14 14:40:12 +0300

    edit admingraph

--- public/adminGraph/index.js
+++ public/adminGraph/index.js
@@ -1,30 +1,34 @@
-async function fetchData() {
-    try {
-      const response = await fetch('/api/statistics');
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error);
-      const errorContainer = document.getElementById('error-container');
-      errorContainer.textContent = 'Error fetching data. Please try again later.';
-      return null;
-    }
-  }
+$(document).ready(function() {
+    renderGraphs();
+  });
   
   async function renderGraphs() {
-      const urlParams = new URLSearchParams(window.location.search);
-      const productId = urlParams.get('id');
-    
-      // Fetch product data
-      const product = await getProduct(productId);
-    
-      if (!product) {
-        return;
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get('id');
+  
+    // Fetch product data
+    const product = await getProduct(productId);
+  
+    if (!product) {
+      return;
+    }
+  
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/statistics');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error(error);
+        const errorContainer = document.getElementById('error-container');
+        errorContainer.textContent = 'Error fetching data. Please try again later.';
+        return null;
       }
-    
+    };
+  
     // Group by category of product
     const categoryData = data.reduce((result, item) => {
       if (!result[item.category]) {
@@ -44,13 +48,13 @@ async function fetchData() {
     js3d.graph('graph1', categoryGraphData, categoryGraphOptions);
   
     const dateData = data.reduce((result, item) => {
-      const date = item.date.substring(0, 10); 
+      const date = item.date.substring(0, 10);
       if (!result[date]) {
         result[date] = 0;
       }
       result[date] += item.sales;
       return result;
-    }, {});s
+    }, {});
   
     const dateGraphData = Object.entries(dateData).map(([date, sales]) => ({ date, sales }));
     const dateGraphOptions = {
@@ -61,11 +65,12 @@ async function fetchData() {
     };
     js3d.graph('graph2', dateGraphData, dateGraphOptions);
   }
+  
   function getProduct(id) {
     return new Promise((resolve, reject) => {
       $.ajax({
         type: 'GET',
-        url:"http://localhost:80/products/"+id,
+        url: 'http://localhost:80/products/' + id,
         success: function (data) {
           resolve(data);
         },
@@ -78,4 +83,21 @@ async function fetchData() {
   }
   
   window.addEventListener('load', renderGraphs);
+  
+  
+  function getPurchaseHistory() {
+    return new Promise(function(resolve, reject) {
+      $.ajax({
+        type: 'GET',
+        url: 'http://localhost:80/purchasehistory',
+        success: function(data) {
+          resolve(data);
+        },
+        error: function(xhr, status, error) {
+          console.error(error);
+          reject(error);
+        },
+      });
+    });
+  }
   
